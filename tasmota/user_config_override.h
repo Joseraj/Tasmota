c6fcf88980350a25d717b3c1980fc09c58fbe44d
Jose <joseraj@gmail.com>
2020-03-21 17:38:37 +0100

    size reduction

--- tasmota/user_config_override.h
+++ tasmota/user_config_override.h
@@ -92,4 +92,34 @@ Examples :
 #define USE_SHUTTER           // Add Shutter support (+6k code)
 #endif
 
+#undef USE_SONOFF_RF                            // Add support for Sonoff Rf Bridge (+3k2 code)
+#undef USE_SONOFF_SC                            // Add support for Sonoff Sc (+1k1 code)
+#undef USE_TUYA_MCU                             // Add support for Tuya Serial MCU
+#undef USE_ARMTRONIX_DIMMERS                    // Add support for Armtronix Dimmers (+1k4 code)
+#undef USE_PS_16_DZ                             // Add support for PS-16-DZ Dimmer (+2k code)
+#undef USE_SONOFF_IFAN                          // Add support for Sonoff iFan02 and iFan03 (+2k code)
+#undef USE_BUZZER                               // Add support for a buzzer (+0k6 code)
+#undef USE_ARILUX_RF                            // Add support for Arilux RF remote controller (+0k8 code, 252 iram (non 2.3.0))
+#undef USE_EXS_DIMMER                           // Add support for ES-Store WiFi Dimmer (+1k5 code)
+#undef USE_PWM_DIMMER                           // Add support for MJ-SD01/acenx/NTONPOWER PWM dimmers (+2k5 code)
+#undef USE_SONOFF_D1                            // Add support for Sonoff D1 Dimmer (+0k7 code)
+#undef USE_WS2812                               // WS2812 Led string using library NeoPixelBus (+5k code, +1k mem, 232 iram) - Disable by //
+#undef USE_MY92X1                               // Add support for MY92X1 RGBCW led controller as used in Sonoff B1, Ailight and Lohas
+#undef USE_SM16716                              // Add support for SM16716 RGB LED controller (+0k7 code)
+#undef USE_SM2135                               // Add support for SM2135 RGBCW led control as used in Action LSC (+0k6 code)
+#undef USE_SONOFF_L1                            // Add support for Sonoff L1 led control
+#undef USE_ELECTRIQ_MOODL                       // Add support for ElectriQ iQ-wifiMOODL RGBW LED controller (+0k3 code)
+#undef USE_COUNTER                              // Enable inputs as counter (+0k8 code)
+#undef USE_DS18x20                              // Add support for DS18x20 sensors with id sort, single scan and read retry (+2k6 code)
+#undef USE_I2C                                  // I2C using library wire (+10k code, 0k2 mem, 124 iram)
+#undef USE_DHT                                  // Add support for DHT11, AM2301 (DHT21, DHT22, AM2302, AM2321) and SI7021 Temperature and Humidity sensor (1k6 code)
+#undef USE_ENERGY_MARGIN_DETECTION              // Add support for Energy Margin detection (+1k6 code)
+#undef USE_PZEM004T                             // Add support for PZEM004T Energy monitor (+2k code)
+#undef USE_PZEM_AC                              // Add support for PZEM014,016 Energy monitor (+1k1 code)
+#undef USE_PZEM_DC                              // Add support for PZEM003,017 Energy monitor (+1k1 code)
+#undef USE_MCP39F501                            // Add support for MCP39F501 Energy monitor as used in Shelly 2 (+3k1 code)
+#undef USE_IR_REMOTE                            // Send IR remote commands using library IRremoteESP8266 and ArduinoJson (+4k3 code, 0k3 mem, 48 iram)
+
+#define FIRMWARE_LITE                            // Create tasmota-lite with no sensors
+
 #endif  // _USER_CONFIG_OVERRIDE_H_
